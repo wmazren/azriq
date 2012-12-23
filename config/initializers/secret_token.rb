3eb12b120fc3a2ab5900faef4650ec950b3b24e8
wmazren <w.m.azren@gmail.com>
2012-12-24 07:40:42 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Azriq::Application.config.secret_token = '70be4aed70e93ab6f21def425b97ebb751d6d5f65d69313a7ee5b919eda4b2809909a82ac2b00f249300647ec14fcf4ccda843d18c46c6d97a70b8e139de7c22'
